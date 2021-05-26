cefae39c99fe6eae9eeca8fabda50d2d8d66d2cd
Jonathan Jacka <jonathanjacka@gmail.com>
2021-05-26 13:42:21 -0500

    Initialized Congrats component and assoc. test file

---
+++ src/components/congrats/Congrats.js
@@ -0,0 +1,16 @@
+/**
+ * Functional react component for congrats message
+ * @function
+ * @param {object} props  - react props
+ * @returns {JSX.Element} - rendered component or 'null' if success prop is false
+ */
+
+import React from 'react';
+
+export const Congrats = ( {success} ) => {
+
+
+    return (
+        <div>Congrats!</div>
+    );
+}
