6fada10347203dee4adeb1bec0d446c8f0d2921c
Jonathan Jacka <jonathanjacka@gmail.com>
2021-05-28 05:56:58 -0500

    Updated GuessedWords.js with table display

--- src/components/guessedWords/GuessedWords.js
+++ src/components/guessedWords/GuessedWords.js
@@ -8,10 +8,25 @@ export const GuessedWords = (props) => {
     if(props.guessedWords.length === 0){
         contents = 'Try to guess the correct word!';
     }else{
+
+        const guessedWordRows = props.guessedWords.map((word, index) => (
+            <tr data-test="guessed-word" key={index}>
+                <td>{word.guessedWord}</td>
+                <td>{word.letterMatchCount}</td>
+            </tr>
+        ));
+
         contents = (
             <div data-test="guessed-words">
-                <h3>Guesses so far</h3>
-                {props.guessedWords.map((item, index) => <div key={index} data-test="guessed-word">{item}</div>)}
+                <h3>Guesses Word</h3>
+                <table>
+                    <thead>
+                        <tr><th>Guess</th><th>Matching Letters</th></tr>
+                    </thead>
+                    <tbody>
+                        {guessedWordRows}
+                    </tbody>
+                </table>
             </div>
         )
     }
