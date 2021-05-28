0935e7316fc9ce1cb5c4dae50c4e7f680c8f1a7b
Jonathan Jacka <jonathanjacka@gmail.com>
2021-05-27 19:14:16 -0500

    Updated GuessedWords.test.js

--- src/components/guessedWords/GuessedWords.test.js
+++ src/components/guessedWords/GuessedWords.test.js
@@ -4,8 +4,28 @@ import { findByTestAttr } from '../../../test/testUtils.js';
 
 import { GuessedWords } from './GuessedWords';
 
+const defaultProps = [{
+    GuessedWords: [{
+        guessedWord: 'train',
+        letterMatchCount: 3
+    }]
+}];
+
+/**
+ * Factory function to create shallow wrapper for the Congrats component
+ * @function setup
+ * @param {object} props - component props specific to this setup
+ * @returns {ShallowWrapper}
+ */
+const setUp = (props = {}) => {
+    const setupProps = {...defaultProps, ...props};
+    return shallow(<GuessedWords {...setupProps}/>);
+}
+
+
+
 test('component renders without crashing', () => {
-    const wrapper = shallow(<GuessedWords />);
+    const wrapper = setUp();
     const component = findByTestAttr(wrapper, 'component-guessedWords');
     expect(component.length).toBe(1);
 });
