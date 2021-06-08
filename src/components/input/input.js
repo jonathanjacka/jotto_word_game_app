01fd72cf1c5497bd0f52520f1d673224786161cc
Jonathan Jacka <jonathanjacka@gmail.com>
2021-06-08 10:19:32 -0500

    Updated input.js and input.test.js to render Input component

---
+++ src/components/input/input.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Proptypes from 'prop-types';
+
+export const Input = (props) => {
+
+    return (
+        <div data-test="component-input"></div>
+    )
+        
+    
+}
+
+Input.propTypes = {
+    
+}
--- src/components/input/input.test.js
+++ src/components/input/input.test.js
@@ -3,9 +3,21 @@ import { findByTestAttr } from '../../../test/testUtils';
 import { shallow } from 'enzyme';
 import { Input } from './input';
 
+/**
+ * Factory function to create shallow wrapper for the Input component
+ * @function setup
+ * @param {Object} props - component props specific to this setup
+ * @returns {ShallowWrapper}
+ */
+
+const setup = (props = {}) => {
+    const setupProps = {...props};
+    return shallow(<Input {...setupProps}/>)
+}
+
 test('renders without crashing', () => {
-    const wrapper = shallow(<Input />);
+    const wrapper = setup();
     const component = findByTestAttr(wrapper, 'component-input');
-    expect(component.length()).toBe(1);
+    expect(component.length).toBe(1);
 });
 
