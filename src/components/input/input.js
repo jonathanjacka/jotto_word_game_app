319153b3ee2cb55c2efa61b75016240a7063ec82
Jonathan Jacka <jonathanjacka@gmail.com>
2021-06-08 21:09:01 -0500

    Updated input.js to complete component structure and state

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -3,8 +3,29 @@ import Proptypes from 'prop-types';
 
 export const Input = ({ secretWord }) => {
 
+    const [currentGuess, setCurrentGuess] = React.useState('');
+
     return (
-        <div data-test="component-input"></div>
+        <div data-test="component-input">
+            
+            <form className="form-inline">
+                <input 
+                    data-test="input-box"
+                    className="mb-2 mx-sm-3"
+                    type="text"
+                    placeholder="Enter guess"
+                    value={currentGuess}
+                    onChange={(event) => setCurrentGuess(event.target.value)}
+                />
+                <button
+                    data-test="submit-button"
+                    className="btn btn-primary mb-2"
+                >
+                Submit
+                </button>
+            </form>
+
+        </div>
     )   
 }
 
