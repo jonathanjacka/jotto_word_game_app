1326a81af9ed31fe2c6d0e9d72624968848a5aba
Jonathan Jacka <jonathanjacka@gmail.com>
2021-06-08 21:28:23 -0500

    Updated input.test.js to check onChange uses setState correctly

--- src/components/input/input.test.js
+++ src/components/input/input.test.js
@@ -21,3 +21,18 @@ test('renders without crashing', () => {
     expect(component.length).toBe(1);
 });
 
+describe('state-controlled input field', () => {
+    test('state updates with value of input box on change', () => {
+        const mockSetCurrentGuess = jest.fn();
+        React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+
+        const wrapper = setup();
+        const inputBox = findByTestAttr(wrapper, 'input-box');
+
+        const mockEvent = { target: { value: 'poopie' } };
+        inputBox.simulate("change", mockEvent);
+
+        expect(mockSetCurrentGuess).toHaveBeenCalledWith('poopie');
+    });
+});
+
