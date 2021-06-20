fdf68de3ad1de96f29827951195775605d47470e
Jonathan Jacka <jonathanjacka@gmail.com>
2021-06-19 19:13:59 -0500

    Setup outline function to getSecretWord and to test with moxios

---
+++ src/actions/index.test.js
@@ -0,0 +1,12 @@
+import moxios from 'moxios';
+
+describe('getWordWord', () => {
+  beforeEach(() => {
+    moxios.install();
+  });
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  test('secretWord os returned', () => {});
+});
