4f61d65cdaeadcde1bf8f2deb1aabca8c4e4f6a4
Jonathan Jacka <jonathanjacka@gmail.com>
2021-06-22 14:56:34 -0500

    Updated testing and functioning of getSecretWord

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import './App.css';
 
+import { useEffect } from 'react';
 import { Congrats } from '../components/congrats/Congrats.js';
 import { GuessedWords } from '../components/guessedWords/GuessedWords.js';
 import { Input } from '../components/input/input.js';
+import { getSecretWord } from '../actions';
 
 function App() {
   const [secretWord, setSecretWord] = useState('party');
@@ -13,6 +15,10 @@ function App() {
   const [success, setSuccess] = useState(false);
   const updateSuccess = (currentGuess) =>
     currentGuess === secretWord ? setSuccess(true) : setSuccess(false);
+
+  useEffect(() => {
+    getSecretWord();
+  }, []);
 
   return (
     <div className='App container' data-test='component-app'>
--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,6 +1,6 @@
 //import { render, screen } from '@testing-library/react';
 import App from './App';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { findByTestAttr } from '../../test/testUtils.js';
 import { getSecretWord as mockGetSecretWord } from '../actions';
 
@@ -13,11 +13,34 @@ jest.mock('../actions');
  * @returns {ShallowWrapper}
  */
 const setup = () => {
-  return shallow(<App />);
+  //need to use mount, b/c useEffect is not run on 'shallow' - https://github.com/enzymejs/enzyme/issues/2086
+  //Package to solve this issue here, though I'm not going to use this rn - https://www.npmjs.com/package/jest-react-hooks-shallow
+  return mount(<App />);
 };
 
 test('renders without error', () => {
   const wrapper = setup();
   const appComponent = findByTestAttr(wrapper, 'component-app');
   expect(appComponent.length).toBe(1);
+});
+
+describe('Get secret Word', () => {
+  beforeEach(() => {
+    //clear the mock calls from previous tests
+    mockGetSecretWord.mockClear();
+  });
+  test('Secret word is retrieved on app mount', () => {
+    const wrapper = setup();
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(1);
+  });
+  test('Secret word is not retrieved on app update', () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+
+    //using setProps b/c wrapper.update() doesn't trigger useEffect
+    //http://github.com/enzymejs/enzyme/issues/2254
+    wrapper.setProps();
+
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(0);
+  });
 });
